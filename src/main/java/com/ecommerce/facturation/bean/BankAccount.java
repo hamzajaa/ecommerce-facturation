a87b9c8a9820a3560bdfc0abf4094cdfed3287f0
WALID ZAANOUN <zaanoun3@gmail.com>
2023-12-06 22:53:31 +0100

    update Bank Account

--- src/main/java/com/ecommerce/facturation/bean/BankAccount.java
+++ src/main/java/com/ecommerce/facturation/bean/BankAccount.java
@@ -2,8 +2,12 @@ package com.ecommerce.facturation.bean;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class BankAccount extends AbstractSupperClass{
     private String rib;
     private String Bank;
