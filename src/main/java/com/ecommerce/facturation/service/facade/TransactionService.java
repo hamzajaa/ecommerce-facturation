357417dad47d3ce5118cee587fdd9938e375cd0b
WALID ZAANOUN <zaanoun3@gmail.com>
2023-12-07 23:34:16 +0100

    update Transaction service

--- src/main/java/com/ecommerce/facturation/service/facade/TransactionService.java
+++ src/main/java/com/ecommerce/facturation/service/facade/TransactionService.java
@@ -1,5 +1,7 @@
 package com.ecommerce.facturation.service.facade;
 
+import com.ecommerce.facturation.dto.CreditDTO;
+import com.ecommerce.facturation.dto.DebitDTO;
 import com.ecommerce.facturation.dto.TransactionDTO;
 
 import java.util.List;
@@ -7,8 +9,10 @@ import java.util.List;
 public interface TransactionService {
     List<TransactionDTO> getTransactions();
     TransactionDTO findById(Long id);
-    TransactionDTO save(TransactionDTO transactionDTO);
-    TransactionDTO update(TransactionDTO transactionDTO);
+    CreditDTO saveCredit(CreditDTO creditDTO);
+    DebitDTO saveDebit(DebitDTO debitDTO);
+    CreditDTO updateCredit(CreditDTO creditDTO,Long id);
+    DebitDTO updateDebit(DebitDTO debitDTO,Long id);
     boolean deleteTransactionById(Long id);
 
 }
