f4b3271fee7fc8266ba3bf4f19faafbe07f81e32
WALID ZAANOUN <zaanoun3@gmail.com>
2023-12-07 19:10:04 +0100

    update UserDTO

--- src/main/java/com/ecommerce/facturation/dto/UserDTO.java
+++ src/main/java/com/ecommerce/facturation/dto/UserDTO.java
@@ -1,12 +1,19 @@
 package com.ecommerce.facturation.dto;
 
+import com.ecommerce.facturation.Enum.Role;
 import com.ecommerce.facturation.bean.BankAccount;
 import com.ecommerce.facturation.bean.BillingToReceive;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
 import java.util.List;
 
 public record UserDTO(
         String fullName,
+        String email,
+        String address,
+        String phoneNumber,
+        Role role,
         List<BankAccountDTO> bankAccountDTOS,
         List<BillingToPayDTO> billingToPayDTOS,
         List<BillingToReceiveDTO> billingToReceiveDTOS
