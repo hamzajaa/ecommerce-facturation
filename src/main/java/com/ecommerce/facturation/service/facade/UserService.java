419829a7e960c3725b8119089911b0c963ce6782
WALID ZAANOUN <zaanoun3@gmail.com>
2023-12-07 19:18:10 +0100

    add User Service

---
+++ src/main/java/com/ecommerce/facturation/service/facade/UserService.java
@@ -0,0 +1,13 @@
+package com.ecommerce.facturation.service.facade;
+
+import com.ecommerce.facturation.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService {
+    List<UserDTO> getUsers();
+    UserDTO findById(Long id);
+    UserDTO save(UserDTO userDTO);
+    UserDTO update(UserDTO userDTO);
+    boolean deleteUserById(Long id);
+}
