3cea4bdd3b7023b2c067f7af80d664d7aca50209
Ayman-Guaiche <guaicheayman@gmail.com>
2023-12-12 10:23:27 +0100

    make some modification on UserDTO and UserMapper

--- src/main/java/com/ecommerce/facturation/dto/UserDTO.java
+++ src/main/java/com/ecommerce/facturation/dto/UserDTO.java
@@ -1,25 +1,12 @@
 package com.ecommerce.facturation.dto;
-
 import com.ecommerce.facturation.Enum.Role;
-import com.ecommerce.facturation.bean.BankAccount;
-import com.ecommerce.facturation.bean.BillingToReceive;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 
 public record UserDTO(
-    @NotNull(message = "ID cannot be null")
     Long id,
 
     @NotBlank(message = "Full name cannot be blank")
